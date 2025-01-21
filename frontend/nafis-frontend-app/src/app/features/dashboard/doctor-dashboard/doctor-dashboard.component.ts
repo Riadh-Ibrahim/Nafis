d08be3fbc594ee1b0ef3a6b540b74d75e6352d96
Wassim Laabidi <wsmlaabidi@gmail.com>
2025-01-21 15:36:35 +0100

    removed a line in doctor dashboard ts

--- frontend/nafis-frontend-app/src/app/features/dashboard/doctor-dashboard/doctor-dashboard.component.ts
+++ frontend/nafis-frontend-app/src/app/features/dashboard/doctor-dashboard/doctor-dashboard.component.ts
@@ -1,4 +1,3 @@
-// features/dashboard/doctor-dashboard/doctor-dashboard.component.ts
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
