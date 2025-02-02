a9f47345d3a452350af4174cc475b4dff9abdf59
khalilkrugerOS <arabickhalil@gmail.com>
2025-02-02 16:45:32 +0100

    ⚡️added side nav + new icons

--- frontend/nafis-frontend-app/src/icons.module.ts
+++ frontend/nafis-frontend-app/src/icons.module.ts
@@ -1,12 +1,73 @@
 import { NgModule } from '@angular/core';
-import { LucideAngularModule, Bell, Menu, Activity, Thermometer, Heart, Save, AlertCircle, X, Home, Info, Package, Phone, DollarSign } from 'lucide-angular';
+import {
+  LucideAngularModule,
+  Bell,
+  Menu,
+  Activity,
+  Thermometer,
+  Heart,
+  Save,
+  AlertCircle,
+  X,
+  Home,
+  Info,
+  Package,
+  Phone,
+  DollarSign,
+  LayoutDashboard,
+  ChartNoAxesCombined,
+  Calendar,
+  Settings2,
+  Users,
+  BriefcaseMedical,
+  ArrowRight,
+  LayoutGrid,
+  UserRoundSearch,
+  Hospital,
+  FileChartColumnIncreasing,
+  Contact,
+  Moon,
+  Sun,
+  LogOut,
+  Ambulance,
+} from 'lucide-angular';
 
 @NgModule({
   imports: [
-    LucideAngularModule.pick({ Bell,Menu,Activity,Thermometer,Heart,Save,AlertCircle,X,Home,Info,Package,Phone,DollarSign }) // Fournit l'icône Bell
+    LucideAngularModule.pick({
+      Bell,
+      Menu,
+      Activity,
+      Thermometer,
+      Heart,
+      Save,
+      AlertCircle,
+      X,
+      Home,
+      Info,
+      Package,
+      Phone,
+      DollarSign,
+      LayoutDashboard,
+      ChartNoAxesCombined,
+      Calendar,
+      Settings2,
+      Users,
+      BriefcaseMedical,
+      ArrowRight,
+      LayoutGrid,
+      UserRoundSearch,
+      Hospital,
+      FileChartColumnIncreasing,
+      Contact,
+      Moon,
+      Sun,
+      LogOut,
+      Ambulance,
+    }), // Fournit l'icône Bell
   ],
   exports: [
-    LucideAngularModule // Exporte le module pour qu'il soit utilisable ailleurs
-  ]
+    LucideAngularModule, // Exporte le module pour qu'il soit utilisable ailleurs
+  ],
 })
 export class IconsModule {}
