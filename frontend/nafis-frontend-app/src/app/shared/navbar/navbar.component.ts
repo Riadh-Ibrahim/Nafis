abf2cf7a594536545554b5122494185e83e76e7a
khalilkrugerOS <arabickhalil@gmail.com>
2025-02-05 16:34:39 +0100

    fixed auth navbar

--- frontend/nafis-frontend-app/src/app/shared/navbar/navbar.component.ts
+++ frontend/nafis-frontend-app/src/app/shared/navbar/navbar.component.ts
@@ -1,36 +1,44 @@
 // navbar.component.ts
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { LucideAngularModule, Menu, X} from 'lucide-angular';
-
+import { LucideAngularModule, Menu, X } from 'lucide-angular';
+import { Store } from '@ngrx/store';
+import * as AuthSelectors from '../../core/store/auth/selectors/auth.selectors';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule,
-    LucideAngularModule,],
-  
-    templateUrl: './navbar.component.html', 
-    styleUrls: ['./navbar.component.scss']
+  imports: [CommonModule, RouterModule, LucideAngularModule],
+
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  constructor(private store: Store) {}
+  isAuthenticated$ = this.store.select(AuthSelectors.selectIsAuthenticated);
+
   isOpen = false;
   showNotifications = false; // Contrôle l'affichage du menu déroulant des notifications
   unreadCount = 2; // Nombre de notifications non lues (à remplacer par une logique dynamique)
   notifications = [
-    { id: 1, message: 'Nouvelle alerte : Température critique détectée.', read: false },
-    { id: 2, message: 'Rappel : Rendez-vous à 10h avec le Dr. Smith.', read: false },
-  ]; 
-
+    {
+      id: 1,
+      message: 'Nouvelle alerte : Température critique détectée.',
+      read: false,
+    },
+    {
+      id: 2,
+      message: 'Rappel : Rendez-vous à 10h avec le Dr. Smith.',
+      read: false,
+    },
+  ];
 
   navigationItems = [
     { name: 'Home', href: '/dashboard', fragment: '' },
     { name: 'About', href: '/landing', fragment: 'about' },
     { name: 'Services', href: '/landing', fragment: 'features' },
-    { name: 'Contact', href: '/landing', fragment: 'testimonials' }
+    { name: 'Contact', href: '/landing', fragment: 'testimonials' },
   ];
 
   // Basculer l'affichage des notifications
@@ -40,7 +48,7 @@ export class NavbarComponent {
 
   // Marquer une notification comme lue
   markAsRead(id: number) {
-    const notification = this.notifications.find(n => n.id === id);
+    const notification = this.notifications.find((n) => n.id === id);
     if (notification && !notification.read) {
       notification.read = true;
       this.unreadCount -= 1;
