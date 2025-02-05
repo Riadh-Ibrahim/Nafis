9c3b815503a9d2a11a7460918c6091ab32baf91f
Riadh <riadhibrahim007@gmail.com>
2025-02-05 21:59:22 +0100

    Add messaging feature's frontend

--- frontend/nafis-frontend-app/src/app/app.component.ts
+++ frontend/nafis-frontend-app/src/app/app.component.ts
@@ -2,14 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { LucideAngularModule } from 'lucide-angular';
 import { IconsModule } from '../icons.module';
-
+import { LucideAngularModule, Send, X, ChevronUp } from 'lucide-angular';
 import { ChatbotComponent } from './features/chatbot/chatbot.component';
 // import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 //import { TemperatureChartComponent } from './features/charts/charts.component';
 //import { RealtimeTemperatureChartComponent } from './shared/realtime-chart/realtime-chart.component';
-
 
 @Component({
   selector: 'app-root',
--- frontend/nafis-frontend-app/src/main.ts
+++ frontend/nafis-frontend-app/src/main.ts
@@ -8,13 +8,18 @@ import { authReducer } from './app/core/store/auth/reducers/auth.reducer';
 import { AuthEffects } from './app/core/store/auth/effects/auth.effects';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MessagingEffects } from './app/core/services/chat/chat.effects';
+import { messagingReducer } from './app/core/services/chat/chat.reducer';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideRouter(routes),
-    provideStore({ auth: authReducer }), 
-    provideEffects([AuthEffects]), provideAnimationsAsync(),     
+    provideStore({ auth: authReducer, }),
+    provideStore({ auth: authReducer, messaging: messagingReducer }), 
+    provideEffects([AuthEffects, MessagingEffects]), 
+    provideEffects([AuthEffects]), provideAnimationsAsync(),
+    
   ],
 }).catch(err => console.error(err));
 
